b439b064924010afe1d8b66481e8fb04df1732c2
lokesh755 <lokeshgopu@NAGARAMALOKESHs-MacBook-Pro.local>
2016-04-07 23:34:20 -0400

    Leadboard UI
    Leaderboard UI changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,78 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+
   model(){
     return {
+      leaderboardData:{
+        dayData:[
+          {
+            name:"Jae Sabol",
+            hours:64,
+            rank:1
+          },
+          {
+            name:"Lokesh Gopu",
+            hours:45,
+            rank:2
+          },
+          {
+            name:"Volunteer 3",
+            hours:38,
+            rank:3
+          },
+          {
+            name:"Volunteer 4",
+            hours:38,
+            rank:4
+          },
+          {
+            name:"Volunteer 5",
+            hours:38,
+            rank:5
+          },
+          {
+            name:"Volunteer 6",
+            hours:38,
+            rank:6
+          },
+          {
+            name:"Volunteer 7",
+            hours:38,
+            rank:7
+          },
+          {
+            name:"Volunteer 8",
+            hours:38,
+            rank:8
+          },
+          {
+            name:"Volunteer 9",
+            hours:38,
+            rank:9
+          },
+          {
+            name:"Volunteer 10",
+            hours:38,
+            rank:10
+          },
+          {
+            name:"Volunteer 11",
+            hours:38,
+            rank:11
+          },
+          {
+            name:"Volunteer 12",
+            hours:38,
+            rank:12
+          },
+          {
+            name:"Volunteer 13",
+            hours:38,
+            rank:13
+          }
+        ]
+      },
       weekData:{
 
         chartOptions:
