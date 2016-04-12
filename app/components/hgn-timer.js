5727215bffe7e4ee670c293473de53774e4f80ab
lokesh755 <lokeshgopu@NAGARAMALOKESHs-MacBook-Pro.local>
2016-04-12 03:04:26 -0400

    Task Clock
    Task Clock

--- app/components/hgn-timer.js
+++ app/components/hgn-timer.js
@@ -2,9 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   clock: Ember.inject.service('hgn-clock'),
-  iso: Ember.computed('clock.second', function() {
-    console.log(this.get('clock.second'));
-    return this.get('clock.minute');
+  seconds: Ember.computed('clock.second', function() {
+      var second = this.get('clock.second');
+      if(second<10)
+        second = "0"+second;
+      return second;
+  }),
+  minutes: Ember.computed('clock.second', function() {
+    var minute = this.get('clock.minute');
+    if(minute<10)
+      minute = "0"+minute;
+    return minute;
+  }),
+  hours: Ember.computed('clock.hour', function() {
+    var hour = this.get('clock.hour');
+    if(hour<10)
+      hour = "0"+hour;
+    return hour;
   }),
   status: 0,
   /*
@@ -14,25 +28,26 @@ export default Ember.Component.extend({
   */
   actionText: "Start",
   stopDisabled: "disabled",
+  actionIcon : "play",
   actions: {
         timerAction: function() {
             if(this.get('status') == 0)
             {
               this.get('clock').start();
-              this.setProperties({status:1, actionText: "Pause", stopDisabled:""});
+              this.setProperties({status:1, actionText: "Pause",actionIcon:"pause", stopDisabled:""});
             }
             else if(this.get('status') == 1)
             {
               this.get('clock').stop();
-              this.setProperties({status:0, actionText: "Start"});
+              this.setProperties({status:0, actionText: "Start", actionIcon:"play"});
             }
 
         },
         resetTimer: function()
         {
             this.get('clock').reset();
-            this.setProperties({status:0, actionText: "Start", stopDisabled:"disabled"});
-          
+            this.setProperties({status:0, actionText: "Start",actionIcon:"play", stopDisabled:"disabled"});
+
         },
         stopTimer: function()
         {
