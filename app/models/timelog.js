0f00e762c2c837e5c2353aa63a1b7449913e19c2
Jono Lewis <hello@jonolewis.com>
2017-05-30 15:33:11 -0700

    Add cross domain handling for REST API, and simplify timelog model

--- app/models/timelog.js
+++ app/models/timelog.js
@@ -3,13 +3,11 @@ let {attr} = DS;
 
 export default DS.Model.extend({
   //profile: DS.belongsTo('profile'),
-  date: attr("date"),
-  startTime: attr('date'),
-  endTime: attr('date'),
+  createDate: attr('date'),
+  lastModifyDate: attr('date'),
   totalSeconds: attr('number'),
   tangible: attr('boolean'),
   workCompleted: attr('string'),
   project: attr('string'),
   task: attr('string')
-
 });
