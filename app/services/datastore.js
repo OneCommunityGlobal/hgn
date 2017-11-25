676b8c090e16fd4cc0b6044e48be5ab6009f0488
Shubhra Mittal <shubhra.goel@gmail.com>
2017-11-24 20:59:26 -0800

    created new service in node.  The service gets data from dashboard. As there is no model for dashboard data, did not use mongoose

--- app/services/datastore.js
+++ app/services/datastore.js
@@ -4,11 +4,17 @@ export default Ember.Service.extend({
 
     
     getDashboardData(){
-        let data = Ember.$.getJSON('/data/data.json');
-        return data;
+        //let data = Ember.$.getJSON('/data/data.json');
 
-
-    }
+        let data = Ember.$.ajax("http://localhost:4500/api/dashboard",
+        {
+            "type" : "get"
+            //"dataType":"JSON"     
+        }  )
+        console.log(data);
+       
+            return data;
+}
                
     
 });
