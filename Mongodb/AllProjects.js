56e3e1e6b5ab7fe411788f95e4d0133e3c75bf5c
Shubhra Mittal <shubhra.goel@gmail.com>
2017-11-28 16:39:52 -0800

    Added first draft of mongodb schema for person, project and time entry

---
+++ Mongodb/AllProjects.js
@@ -0,0 +1,6 @@
+db.AllProjects.insert([
+{"ProjectName" : "Project1", IsActive: 1, Tasks : [{Id :1, Description:"Eating"}, {Id :2, Description:"Eating"}, {Id :3, Description:"Playing"}]},
+
+{"ProjectName" : "Project2", IsActive: 0, Tasks : [{Id :1, Description:"Skiing"}, {Id :2, Description:"Driving"}, {Id :3, Description:"Rowing"}]}
+
+])
---
+++ Mongodb/TimeEntry.js
@@ -0,0 +1,33 @@
+db.TimeEntry.insert(
+[
+{personId : "5a1df1d49831328f3d217ec9", 
+  "week": "week_01_21_2017", 
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f88", TaskId: 1, TimeLogged : "2:3:3"}, 
+                     {projectId : "5a1dfc8d474446d144a24f88", TaskId: 3, TimeLogged : "1:0:0"}	],
+  
+  
+  
+    "week": "week_01_21_2018", 
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f89", TaskId: 4, TimeLogged : "2:3:3"}, 
+                     {projectId : "5a1dfc8d474446d144a24f89", TaskId: 3, TimeLogged : "1:0:0"}	]
+    
+},
+
+{personId : "2", 
+  "week": "week_01_21_2016", 
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f88", TaskId: 1, TimeLogged : "2:3:3"}, 
+                     {projectId : "5a1dfc8d474446d144a24f88", TaskId: 3, TimeLogged : "1:0:0"}	],
+  
+  
+  
+    "week": "week_01_21_2016", 
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f89", TaskId: 4, TimeLogged : "2:3:3"}, 
+                     {projectId : "5a1dfc8d474446d144a24f89", TaskId: 3, TimeLogged : "1:0:0"}	]
+    
+}
+
+
+
+]
+
+)
