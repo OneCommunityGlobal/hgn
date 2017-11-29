6d032c0413e9344df4446ec1dc5af68db25ee820
Shubhra Mittal <shubhra.goel@gmail.com>
2017-11-28 16:49:22 -0800

    Added timestamp and notes to time entry

--- Mongodb/TimeEntry.js
+++ Mongodb/TimeEntry.js
@@ -2,27 +2,27 @@ db.TimeEntry.insert(
 [
 {personId : "5a1df1d49831328f3d217ec9", 
   "week": "week_01_21_2017", 
-    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f88", TaskId: 1, TimeLogged : "2:3:3"}, 
-                     {projectId : "5a1dfc8d474446d144a24f88", TaskId: 3, TimeLogged : "1:0:0"}	],
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f88", TaskId: 1, TimeLogged : "2:3:3",notes : "Some notes here", createdDateTime: new Date()}, 
+                     {projectId : "5a1dfc8d474446d144a24f88", TaskId: 3, TimeLogged : "1:0:0", createdDateTime: new Date()}	],
   
   
   
     "week": "week_01_21_2018", 
-    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f89", TaskId: 4, TimeLogged : "2:3:3"}, 
-                     {projectId : "5a1dfc8d474446d144a24f89", TaskId: 3, TimeLogged : "1:0:0"}	]
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f89", TaskId: 4, TimeLogged : "2:3:3", createdDateTime: new Date()}, 
+                     {projectId : "5a1dfc8d474446d144a24f89", TaskId: 3, TimeLogged : "1:0:0",createdDateTime: new Date()}	]
     
 },
 
 {personId : "2", 
   "week": "week_01_21_2016", 
-    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f88", TaskId: 1, TimeLogged : "2:3:3"}, 
-                     {projectId : "5a1dfc8d474446d144a24f88", TaskId: 3, TimeLogged : "1:0:0"}	],
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f88", TaskId: 1, TimeLogged : "2:3:3", createdDateTime: new Date()}, 
+                     {projectId : "5a1dfc8d474446d144a24f88", TaskId: 3, TimeLogged : "1:0:0", createdDateTime: new Date()}	],
   
   
   
     "week": "week_01_21_2016", 
-    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f89", TaskId: 4, TimeLogged : "2:3:3"}, 
-                     {projectId : "5a1dfc8d474446d144a24f89", TaskId: 3, TimeLogged : "1:0:0"}	]
+    "TimeEnteries" :[{projectId : "5a1dfc8d474446d144a24f89", TaskId: 4, TimeLogged : "2:3:3", createdDateTime: new Date()}, 
+                     {projectId : "5a1dfc8d474446d144a24f89", TaskId: 3, TimeLogged : "1:0:0", createdDateTime: new Date()}	]
     
 }
 
