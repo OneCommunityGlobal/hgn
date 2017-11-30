12eb72d60d24b186b34bc0e73178f296f63e12b0
Sowmya Manohar <sowmi.sarojini@gmail.com>
2017-11-29 19:45:05 -0500

    Added getbyID and updatebyID functions [get operation and put operation] for User model

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,23 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import isEnabled from 'ember-data/-private/features';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
     namespace: 'api',
     authorizer: 'authorizer:oauth2',
-    host: 'http://localhost:4500'
+    host: 'http://localhost:4500',
+    updateRecord(store, type, snapshot) {
+    if (isEnabled('ds-improved-ajax') && !this._hasCustomizedAjax()) {
+      return this._super(...arguments);
+    } else {
+      let data = {};
+      let serializer = store.serializerFor(type.modelName);
+
+      serializer.serializeIntoHash(data, type, snapshot, { includeId: true });
+
+      let url = this.buildURL(type.modelName, snapshot.id, snapshot, 'updateRecord');
+
+      return this.ajax(url, "PUT", { data: data });
+    }
+  }
 });
