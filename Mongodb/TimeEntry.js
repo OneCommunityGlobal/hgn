025c40b4ead3199b234cfdcd187f5e4e6a324347
shubmittal <shubhra.goel@gmail.com>
2017-12-04 10:51:52 -0800

    Update TimeEntry.js
    Added dateOfWork and fixed casing of all fields.

--- Mongodb/TimeEntry.js
+++ Mongodb/TimeEntry.js
@@ -1,8 +1,8 @@
 db.TimeEntry.insert(
 [
-{personId : "5a1df1d49831328f3d217ec9", projectId : "5a1dfc8d474446d144a24f88", TaskId: 1, TimeLogged : "2:3:3",notes : "Some notes here", createdDateTime: new Date(), rollupweek: "03-23-2017", rollupmonth: "May", rollupyear: 2017}, 
+{personId : "5a1df1d49831328f3d217ec9", projectId : "5a1dfc8d474446d144a24f88", taskId: 1, dateofWork: new Date(), timeLogged : "2:3:3",notes : "Some notes here", createdDateTime: new Date(), rollupWeek: "03-23-2017", rollupMonth: "May", rollupYear: 2017}, 
 
-{personId : "5a1df1d49831328f3d217ec9", projectId : "1234", TaskId: 2, TimeLogged : "2:3:3",notes : "Some new notes notes here", createdDateTime: new Date(), rollupweek: "03-23-2017", rollupmonth: "May", rollupyear: 2017} 
+{personId : "5a1df1d49831328f3d217ec9", projectId : "5a1dfc8d474446d144a24f88", taskId: 3, dateofWork: new Date(), timeLogged : "2:3:3",notes : "Some notes here", createdDateTime: new Date(), rollupWeek: "03-23-2017", rollupMonth: "May", rollupYear: 2017} 
                      
 ]
 
