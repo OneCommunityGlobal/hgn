a030e3191f92636bd734b21fa11ddee00fe445bd
shubmittal <shubhra.goel@gmail.com>
2017-12-04 10:53:59 -0800

    Update TimeEntry.js
    Update name of collection to match naming convention

--- Mongodb/TimeEntry.js
+++ Mongodb/TimeEntry.js
@@ -1,4 +1,4 @@
-db.TimeEntry.insert(
+db.timeEntry.insert(
 [
 {personId : "5a1df1d49831328f3d217ec9", projectId : "5a1dfc8d474446d144a24f88", taskId: 1, dateofWork: new Date(), timeLogged : "2:3:3",notes : "Some notes here", createdDateTime: new Date(), rollupWeek: "03-23-2017", rollupMonth: "May", rollupYear: 2017}, 
 
