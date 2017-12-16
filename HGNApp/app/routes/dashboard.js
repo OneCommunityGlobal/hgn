caeecc8e4a57c9b6bf8aaafcc3ae28981227b8e8
2pkothari <2pkothari@gmail.com>
2017-12-16 13:09:00 -0500

    Testing my GitHub setup
    Just added a moment "Testing"

--- HGNApp/app/routes/dashboard.js
+++ HGNApp/app/routes/dashboard.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-
+//Tesing
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
   model(){
-    
+
     var data = this.get('datastore').getDashboardData();
-    
-    
+
+
     return data;
     }
 });
