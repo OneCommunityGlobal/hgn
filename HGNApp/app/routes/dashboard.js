4a56c5f9aadf451d3a8fd18646802626e71ecadb
2pkothari <2pkothari@gmail.com>
2017-12-16 13:10:47 -0500

    Spelling Correction
    Correct the spelling in Testing

--- HGNApp/app/routes/dashboard.js
+++ HGNApp/app/routes/dashboard.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-//Tesing
+//Testing
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
   model(){
