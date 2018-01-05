8183154fdb52e21c0a4b8cd64fa052ae8e3ca573
Shubhra Mittal <shubhra.goel@gmail.com>
2018-01-05 13:34:25 -0800

    Changes to profile

--- HGNApp/app/controllers/myprofile.js
+++ HGNApp/app/controllers/myprofile.js
@@ -5,18 +5,17 @@ export default Ember.Controller.extend({
     userrole : "",
     userId : "",
     self : this,
+    
     isUserAdministrator : Ember.computed('userrole', function(){
 
         let userrole = this.get('userrole');
-        return userrole == "Administrator" ? true: false;
+        return userrole === "Administrator" ? true: false;
 
     }),
     actions: {
 
 
         postChanges(){
-            alert("post triggered!!");
-
             let userId = this.get('userId');
             let user = this.get('model');
 
