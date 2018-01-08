d0f73f11706af9f48c84d02dd8c33e4947e97039
Shubhra Mittal <shubhra.goel@gmail.com>
2018-01-08 09:45:08 -0800

    Added logic to mixin so that the user role and user id are always available to controller

--- HGNApp/app/controllers/myprofile.js
+++ HGNApp/app/controllers/myprofile.js
@@ -2,69 +2,79 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-   
-    self : this,
-    
-    isUserAdministrator : Ember.computed('userrole', function(){
 
-        let userrole = this.get('userrole');
-        return userrole === "Administrator" ? true: false;
+  self: this,
 
-    }),
+  isUserAdministrator: Ember.computed('userrole', function () {
 
-    isFormValid : function(){
+    let userrole = this.get('userrole');
+    return userrole === "Administrator" ? true : false;
 
-        let isFormValid = true;
-        let errors = [];
+  }),
 
-        let firstName = this.get("firstName");
-        alert(firstName);
-        let lastName = this.get("lastName");
+  isFormValid: function (user) {
 
-        if(firstName.length <3)
-           { isFormValid = false;
-              errors.push("First Name is mandatory");
-        }
-        if(lastName.length <3)
-           { isFormValid = false;
-              errors.push("Last Name is mandatory");
-        }
+    let isFormValid = true;
+    let errors = [];
 
-        return {isFormValid, errors};
-
-        
-
-
-    },
-
-    actions: {
-
-
-        postChanges(){
-            let userId = this.get('userId');
-            let user = this.get('model');
-
-            let {isFormValid, errors} = this.isFormValid();
-
-            if(isFormValid)
-            {
-                this.get('DataService').saveUserProfileData(user,userId)
-            .done(alert("Changes Saved!!!"))
-            .fail((error) => alert (error));
-
-            }
-            else
-            {
-                alert("Please fix errors" +errors);
-
-            }
-        },
-        discardChanges(){
-            this.self.transitionToRoute('dashboard');
-
-        }
+    if(user.firstName.length ===0 || user.firstName == null)
+    {
+        isFormValid = false;
+        errors.push("First Name is a mandatory field");
     }
-  
-    
-    
-    });
+    if(user.firstName.length ===0 || user.firstName == null)
+    {
+        isFormValid = false;
+        errors.push("First Name is a mandatory field");
+    }
+
+    if(user.email.length ===0 || user.firstName == null)
+    {
+        isFormValid = false;
+        errors.push("First Name is a mandatory field");
+    }
+
+
+
+
+    return {
+      isFormValid,
+      errors
+    };
+
+
+
+
+  },
+
+  actions: {
+
+
+    postChanges() {
+      let userId = this.get('userId');
+      let user = this.get('model');
+
+      let {
+        isFormValid,
+        errors
+      } = this.isFormValid(user);
+
+      if (isFormValid) {
+        this.get('DataService').saveUserProfileData(user, userId)
+          .done(alert("Changes Saved!!!"))
+          .fail((error) => alert(error));
+
+      } else {
+        alert("Please fix errors" + errors);
+
+      }
+    },
+    discardChanges() {
+      this.self.transitionToRoute('dashboard');
+
+    }
+  }
+
+
+
+});
