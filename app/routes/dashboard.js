a4198e9c4f47048c8e1b7f341babefb47fef975d
2pkothari <2pkothari@gmail.com>
2018-01-08 18:18:17 -0500

    Testing my gitHub integration. Added a comment

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-
+//we need to remove this hardcoded data
   model(){
     return {
       leaderboardData:{
