9ce9a47d0d4586d640625dfc6c7325cfe8526d66
Shubhra Mittal <shubhra.goel@gmail.com>
2018-01-19 10:08:28 -0800

    removed username so as to enable login by email

--- HGNApp/app/services/login-service.js
+++ HGNApp/app/services/login-service.js
@@ -35,7 +35,7 @@ export default Ember.Service.extend({
       url : this.host + "/login" ,
       type: "GET",
       cache: "true",
-      beforeSend: function(xhr){xhr.setRequestHeader("Authorization", localStorage.getItem(ENV.TOKEN_KEY) );}
+      beforeSend: function(xhr){xhr.setRequestHeader(ENV.REQUEST_AUTHKEY, localStorage.getItem(ENV.TOKEN_KEY) );}
     });    
 
   },
