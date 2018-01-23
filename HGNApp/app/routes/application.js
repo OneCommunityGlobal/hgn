0d13e01936a21cf3f95621173736d1dd3c959f5c
Sowmya Manohar <sowmi.sarojini@gmail.com>
2018-01-23 08:37:27 +0530

    Edited the menu tabs to load based on roles

--- HGNApp/app/routes/application.js
+++ HGNApp/app/routes/application.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
-//import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import UnAuthenticatedRouteMixin from '../mixins/un-authenticated-route-mixin';
 
-export default Ember.Route.extend();
+export default Ember.Route.extend(UnAuthenticatedRouteMixin, {
+    
+});
