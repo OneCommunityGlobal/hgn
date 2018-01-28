11415d0dec7e4e7ffca523cd27d5b64959e3be12
Sowmya Manohar <sowmi.sarojini@gmail.com>
2018-01-28 08:00:14 +0530

    Reverting the commit to overcome assertion error

--- HGNApp/app/controllers/application.js
+++ HGNApp/app/controllers/application.js
@@ -5,13 +5,7 @@ export default Ember.Route.extend({
     hideHeaderRoutes: ['index', 'login'],
     isHeaderVisible: Ember.computed('currentRouteName', function () {
       return this.get('hideHeaderRoutes').indexOf(this.get('currentRouteName')) === -1;
-    }),
-
-    isUserAdministrator: Ember.computed('userrole', function () {
-      let userrole = this.get('userrole');
-      return userrole === "Administrator" ? true : false;
-
-    }),
+    })
 
   }
 );
--- HGNApp/app/routes/application.js
+++ HGNApp/app/routes/application.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import UnAuthenticatedRouteMixin from '../mixins/un-authenticated-route-mixin';
+//import UnAuthenticatedRouteMixin from '../mixins/un-authenticated-route-mixin';
 
-export default Ember.Route.extend(UnAuthenticatedRouteMixin, {
+export default Ember.Route.extend({
     
 });
