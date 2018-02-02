460772cf77f81408ba3d454b95f6f11b8f2a801b
Sowmya Manohar <sowmi.sarojini@gmail.com>
2018-02-02 08:23:58 +0530

    Added logic to retrieve all userprofiles by admin from db

--- HGNApp/app/services/user-profile-service.js
+++ HGNApp/app/services/user-profile-service.js
@@ -5,23 +5,31 @@ export default Ember.Service.extend({
 
   dataService: Ember.inject.service('datastore-service'),
 
-    getUserProfileData(requestor) {
+  getAllUserProfiles() {
+    let relativePath = "/userprofile";
+    let data = "";
+    let method = "get";
 
-        let relativePath =  "/userprofile/" + requestor.requestorId;
-        let data = "";
-        let method = "get";
-    
-        let request = this.get('dataService').createEmberrequestObject(relativePath, data, method);
-        return request;
-    
-      },
-      editUserProfileData(user, userId) {
-    
-        let relativePath =  "/userprofile/" + userId;
-        let data = user;
-        let method = "put";
-    
-        let request = this.get('dataService').createEmberrequestObject(relativePath, data, method);
-        return request;
-      },
+    let request = this.get('dataService').createEmberrequestObject(relativePath, data, method);
+    return request;
+  },
+  getUserProfileData(requestor) {
+
+    let relativePath = "/userprofile/" + requestor.requestorId;
+    let data = "";
+    let method = "get";
+
+    let request = this.get('dataService').createEmberrequestObject(relativePath, data, method);
+    return request;
+
+  },
+  editUserProfileData(user, userId) {
+
+    let relativePath = "/userprofile/" + userId;
+    let data = user;
+    let method = "put";
+
+    let request = this.get('dataService').createEmberrequestObject(relativePath, data, method);
+    return request;
+  },
 });
